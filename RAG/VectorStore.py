88012e319f92839c7e1da29694132c87b0ad8b0c
krishnapal <krishnapalkp05@gmail.com>
2026-03-02 12:56:26 +0000

    VectorStore

---
+++ RAG/VectorStore.py
@@ -0,0 +1,53 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.schema import Document
+
+# Create LangChain documents for IPL players
+
+doc1 = Document(
+        page_content="Virat Kohli is one of the most successful and consistent batsmen in IPL history. Known for his aggressive batting style and fitness, he has led the Royal Challengers Bangalore in multiple seasons.",
+        metadata={"team": "Royal Challengers Bangalore"}
+    )
+doc2 = Document(
+        page_content="Rohit Sharma is the most successful captain in IPL history, leading Mumbai Indians to five titles. He's known for his calm demeanor and ability to play big innings under pressure.",
+        metadata={"team": "Mumbai Indians"}
+    )
+doc3 = Document(
+        page_content="MS Dhoni, famously known as Captain Cool, has led Chennai Super Kings to multiple IPL titles. His finishing skills, wicketkeeping, and leadership are legendary.",
+        metadata={"team": "Chennai Super Kings"}
+    )
+doc4 = Document(
+        page_content="Jasprit Bumrah is considered one of the best fast bowlers in T20 cricket. Playing for Mumbai Indians, he is known for his yorkers and death-over expertise.",
+        metadata={"team": "Mumbai Indians"}
+    )
+doc5 = Document(
+        page_content="Ravindra Jadeja is a dynamic all-rounder who contributes with both bat and ball. Representing Chennai Super Kings, his quick fielding and match-winning performances make him a key player.",
+        metadata={"team": "Chennai Super Kings"}
+    )
+
+docs = [doc1, doc2, doc3, doc4, doc5]
+vector_store = Chroma(
+    embedding_function=OpenAIEmbeddings(),
+    persist_directory='my_chroma_db', #file location where this vectores will stores and stored in sqlite form
+    collection_name='sample'
+)
+
+#add documents
+vector_store.add_documents(docs)
+
+# view documents
+vector_store.get(include=['embeddings','documents', 'metadatas'])
+
+# search documents
+vector_store.similarity_search(
+    query='Who among these are a bowler?',
+    k=2  # how many results we need to show or how many similar result need to show
+)
+
+# Metadata filtering
+vector_store.similaritySearch("query", 2, { "team": "Chennai Super Kings" })
+# meta-data filtering
+vector_store.similarity_search_with_score(
+    query="",
+    filter={"team": "Chennai Super Kings"}
+)
